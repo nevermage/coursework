49a10aed19f8788b55e81a57fc170dfe16328a76
binunov <maksym.bihunov@nixsolutions.com>
2022-12-27 16:54:06 +0200

    fix typo

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import paramiko
 
+# @todo move to .env
 host = '0.0.0.0'
 user = 'root'
 secret = 'secret'
 port = 20022
+logPath = '/var/www/logs/error.log'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=host, username=user, password=secret, port=port)
 sftp_client = client.open_sftp()
-file = sftp_client.open("/var/www/logs/error.log")
+file = sftp_client.open(logPath)
 Lines = file.readlines()
 
 for line in Lines:
