b197cd8078949ad5dd97154d8c276a7e26a90d67
binunov <maksym.bihunov@nixsolutions.com>
2022-12-27 16:32:08 +0200

    add readme, fix typo

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ port = 20022
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print(3)
+client.connect(hostname=host, username=user, password=secret, port=port)
 sftp_client = client.open_sftp()
 file = sftp_client.open("/var/www/logs/error.log")
 Lines = file.readlines()
-# Strips the newline character
+
 for line in Lines:
     print(line.strip())
 client.close()
